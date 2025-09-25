2b653db392df95a2b309c7c1f19eef57aadf236b
Lecinel <lsh3000h@gmail.com>
2025-09-25 15:56:31 +0900

    first commit

--- web_study/main/urls.py
+++ web_study/main/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('blog', views.blog, name="blog"),
     path('blog/<int:pk>/',views.posting, name="posting"),
-    path('blog/new_post/', views.new_post, name = "new_post"),
+    path('blog/new_post/', views.new_post),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
